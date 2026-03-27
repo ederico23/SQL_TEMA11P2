08a142d0c373699c2fdc55472ddc7847f06b8986
eder23 <edergracia006@gmail.com>
2026-03-27 16:46:34 +0100

    .

---
+++ Conexion_tema11_parte2.sql
@@ -0,0 +1,7 @@
+/*9. Conectarse como usuario SYSTEM a la base de datos de Oracle y crear un
+usuario llamado “administrador” autentificado por la base de datos con
+contraseña admin. Indicar como "tablespace" por defecto USERS y como
+"tablespace" temporal TEMP; asignar una cuota de 500K en el "tablespace"
+USERS.
+*/
+
